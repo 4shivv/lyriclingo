7652481341082638afb7bb98febf4b098f782c24
4shivv <122394728+4shivv@users.noreply.github.com>
2025-02-13 10:23:51 -0500

    LyricLingo Logo Updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,7 +45,7 @@ function Navbar({ isLoggedIn, setIsLoggedIn }) {
     <nav className="navbar">
       <Link to="/" className="logo-container" onClick={() => setIsMobileMenuOpen(false)}>
         <img src="/IMG_0862.png" alt="LyricLingo Logo" className="logo-image" />
-        <span className="logo-text">LyricLingo</span>
+        <span className="logo-text"></span>
       </Link>
 
       {/* Desktop Menu: Always visible on larger screens */}
