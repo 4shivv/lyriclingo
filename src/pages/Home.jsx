fae47310c53f400278550fb96f2101aa69941e83
4shivv <122394728+4shivv@users.noreply.github.com>
2025-03-11 23:44:09 -0400

    final

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -134,7 +134,7 @@ function Home() {
                   whileHover={{ y: -5, scale: 1.1 }}
                   transition={{ type: "spring", stiffness: 400 }}
                 >
-                  <img src="/email.png" alt="GitHub" className="social-icon" />
+                  <img src="/mail-227.png" alt="GitHub" className="social-icon" />
                 </motion.a>
                 <motion.a 
                   href="https://www.linkedin.com/in/shivaganesh-nagamandla" 
